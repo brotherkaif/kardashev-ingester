31d8611881799f1329115b2bf9156f77e57d98d5
Kaif Ashek Ahmed <hello@kaif.me>
2017-09-13 15:45:11 +0000

    Changed the way JSON files were being parsed and used. Now the user only needs to supply 3 files (audio, video and a JSON) to the script. This will simplify the workflow significantly.

--- create_video.sh
+++ create_video.sh
@@ -6,31 +6,26 @@ ARG_LENGTH="-shortest"
 META_COMMENT=$"[ATTRIBUTION DECLARATION]
 This work, \"$( jq -r '.file.title' $INPUT_METADATA )\", is a derivative of the works listed below. \"$( jq -r '.file.title' $INPUT_METADATA )\" was created by $( jq -r '.file.author' $INPUT_METADATA ) and is licenced under a $( jq -r '.file.licence' $INPUT_METADATA ) licence.
 
-
 [AUDIO ATTRIBUTION]
 Track: $( jq -r '.file.audio_stream.title' $INPUT_METADATA )
 Artist: $( jq -r '.file.audio_stream.author' $INPUT_METADATA )
 Licence: $( jq -r '.file.audio_stream.licence' $INPUT_METADATA )
 Modifications: $( jq -r '.file.audio_stream.modifications' $INPUT_METADATA )
-
-(further audio information)
+Reference URLs:
 $( jq -r '.file.audio_stream.source_URI' $INPUT_METADATA )
 $( jq -r '.file.audio_stream.author_URI' $INPUT_METADATA )
 $( jq -r '.file.audio_stream.licence_URI' $INPUT_METADATA )
-
 
 [VIDEO ATTRIBUTION]
 Track: $( jq -r '.file.video_stream.title' $INPUT_METADATA )
 Artist: $( jq -r '.file.video_stream.author' $INPUT_METADATA )
 Licence: $( jq -r '.file.video_stream.licence' $INPUT_METADATA )
 Modifications: $( jq -r '.file.video_stream.modifications' $INPUT_METADATA )
-
-(further video information)
+Reference URLs:
 $( jq -r '.file.video_stream.source_URI' $INPUT_METADATA )
 $( jq -r '.file.video_stream.author_URI' $INPUT_METADATA )
 $( jq -r '.file.video_stream.licence_URI' $INPUT_METADATA )"
 
-echo "$META_COMMENT"
 
 # Run the muxing job via FFmpeg.
 ffmpeg  -ss $( jq -r '.edit_info.start_timecode' $INPUT_METADATA ) \
