1bbccd80040c2c7896cd4155b445dbab05833552
Kaif Ahmed <hello@kaif.me>
2017-10-02 16:48:21 +0100

    Finished off implementation of batch processing mode.

--- KI.sh
+++ KI.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 # Ingesting script that uses FFmpeg to convert input video and audio into a standardised 1:1 format.
 
+function_process () {
+    echo Processing $INPUT_METADATA ...
+    jq -r '.file' $INPUT_DIRECTORY/$INPUT_METADATA > $INPUT_DIRECTORY/../output/$( jq -r '.file.title' $INPUT_DIRECTORY/$INPUT_METADATA ).json
+    export INPUT_DIRECTORY
+    export INPUT_METADATA
+    export DRY_RUN
+    # ./create_SRT_titles.sh
+    ./create_ASS_titles.sh
+    ./create_video.sh
+    echo $INPUT_FILE done!
+}
+
 # START OF SCRIPT
 # ===============
 
+SINGLE=FALSE
+BATCH=FALSE
 DRY_RUN=FALSE
 
 # Get input arguements and assign them to variables.
-while getopts ":i:d" opt;
+while getopts ":i:b:d" opt;
 do
     case $opt in
         i)
             INPUT_DIRECTORY=$( dirname $OPTARG )
             INPUT_METADATA=$( echo ${OPTARG##*/} )
+            SINGLE=TRUE
+            ;;
+        b)
+            INPUT_DIRECTORY=$OPTARG
+            BATCH=TRUE
             ;;
         d)
-            echo DRY RUN MODE
-            echo ============
-            echo Output file will be limited to 20 seconds.
             DRY_RUN=TRUE
             ;;
         \?)
@@ -31,22 +47,62 @@ do
     esac
 done
 
-# If no arguments passed through, exit gracefully.
-if [ -z "$INPUT_METADATA" ]
+if [ $SINGLE == 'TRUE' ] && [ $BATCH == 'TRUE' ]
 then
-    echo 'You need to supply a JSON file with -i.' >&2
+    echo 'You cannot run SINGLE MODE (-i) and BATCH MODE (-b) at the same time!' >&2
+    exit 1
+elif [ $SINGLE == 'TRUE' ] && [ -z "$INPUT_DIRECTORY" ]
+then
+    echo 'You need to supply a JSON file with (-i).' >&2
+    exit 1
+elif [ $BATCH == 'TRUE' ] && [ -z "$INPUT_DIRECTORY" ]
+then
+    echo 'You need to supply a valid directory with (-b).' >&2
     exit 1
 fi
 
-mkdir -p $INPUT_DIRECTORY/../output
-jq -r '.file' $INPUT_DIRECTORY/$INPUT_METADATA > $INPUT_DIRECTORY/../output/$( jq -r '.file.title' $INPUT_DIRECTORY/$INPUT_METADATA ).json
+if [ $SINGLE == 'TRUE' ]
+then
+    echo SINGLE MODE
+    echo ===========
+    echo The file $INPUT_METADATA will be processed...
+    echo
+elif [ $BATCH == 'TRUE' ]
+then
+    echo BATCH MODE
+    echo ==========
+    echo The following files will be processed...
+    echo
+    for INPUT_FILE in $INPUT_DIRECTORY/*.json
+    do
+        echo $INPUT_FILE
+    done
+    echo
+fi
 
-export INPUT_DIRECTORY
-export INPUT_METADATA
-export DRY_RUN
-#./create_SRT_titles.sh
-./create_ASS_titles.sh
-./create_video.sh
+if [ $DRY_RUN == 'TRUE' ]
+then
+    echo DRY RUN MODE
+    echo ============
+    echo Output will be limited to 20 seconds.
+    echo
+fi
+
+read -p 'Do you want to proceed? (y/n): ' RUN_CONVERSION
+mkdir -p $INPUT_DIRECTORY/../output
+
+if [ $RUN_CONVERSION == 'y' ] && [ $BATCH == 'TRUE' ]
+then
+    for INPUT_FILE in $INPUT_DIRECTORY/*.json
+    do
+        INPUT_DIRECTORY=$( dirname $INPUT_FILE )
+        INPUT_METADATA=$( echo ${INPUT_FILE##*/} )
+        function_process
+    done
+elif [ $RUN_CONVERSION == 'y' ] && [ $SINGLE == 'TRUE' ]
+then
+    function_process
+fi
 
 # =============
 # END OF SCRIPT
